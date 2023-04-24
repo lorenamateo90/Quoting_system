c1908b40ee12bdf9ea87ea0c67253a3b146124e4
lorenamateo90 <lorena.mateo.garcia@gmail.com>
2023-04-24 17:00:33 +0200

    calculate the insurance

--- js/app.js
+++ js/app.js
@@ -1,15 +1,58 @@
 // constructors
-
 function Insurance(brand, year, type){
     this.brand = brand;
     this.year = year;
     this.type = type;
 }
 
+// make the quotation with data
+Insurance.prototype.quoteInsurance = function(){
+    /*
+        1 = Volkswagen 1.15
+        2 = Hyundai 1.05
+        3 = Ford 1.3
+    */
+
+    let amount;
+    const base = 1000;
+
+    switch(this.brand){
+        case '1':
+            amount = base * 1.15;
+            break;
+        case '2':
+            amount = base * 1.05;
+            break;
+        case '3': 
+            amount = base * 1.3;
+            break;
+
+            default: 
+                break;
+    }
+
+    // read year
+    const difference = new Date().getFullYear() - this.year;
+
+    // each year that the price is higher, the value of the insurance is reduced by 3%.
+    amount -= ((difference * 3) * amount) / 100;
+
+    /*
+        if the insurance is basic it's multiplied by 30%
+        if the insurance is complete, it's multiplied by 50%
+    */
+
+    if(this.type === 'basico') {
+        amount *= 1.30;
+    } else {
+        amount *= 1.50;
+    }
+    return amount;
+}
+
 function UI(){}
 
 // fill in the year options
-
 UI.prototype.fillOptions = () => {
     const max = new Date().getFullYear();
           min = max - 20;
@@ -25,14 +68,13 @@ UI.prototype.fillOptions = () => {
 }
 
 // shows alert on screen
-
 UI.prototype.showMessage = (message, type) => {
     const div = document.createElement('div');
 
     if(type === 'error'){
-        div.classList.add('message', 'error');
+        div.classList.add('error');
     }else {
-        div.classList.add('message', 'correct')
+        div.classList.add('sucess')
     }
     div.classList.add('message', 'mt-10');
     div.textContent = message;
@@ -46,10 +88,52 @@ UI.prototype.showMessage = (message, type) => {
     }, 3000);
 }
 
-// instantiate ui
+UI.prototype.showResult = ( insurance, total ) => {
 
+    const { brand, year, type } = insurance;
+    let brandText;
+
+    switch(brand) {
+        case '1':
+            brandText = 'Volkswagen';
+            break;
+        case '2':
+            brandText = 'Hyundai';
+            break;
+        case '3':
+            brandText = 'Ford';
+            break;
+        default:
+            break;
+    }
+
+
+    // create the result
+    const div = document.createElement('div');
+    div.classList.add('mt-10');
+
+    div.innerHTML = `
+    <p class= 'header'>Tu resumen</p>
+    <p class= 'font-bold'>Marca: <span class="font-normal">${brandText}</span></p>
+    <p class= 'font-bold'>Año: <span class="font-normal">${year}</span></p>
+    <p class= 'font-bold'>Tipo: <span class="font-normal">${type}</span></p>
+    <p class= 'font-bold'>Total: <span class="font-normal"> $ ${total}</span></p>
+    `;
+
+    const resultDiv = document.querySelector('#result');
+ 
+    // show spinner
+    const spinner = document.querySelector('#loading');
+    spinner.style.display = 'block';
+
+    setTimeout(() =>{
+        spinner.style.display = 'none'; // spinner is deleted
+        resultDiv.appendChild(div); // the result
+    },3000)
+}
+
+// instantiate ui
 const ui = new UI();
-console.log(ui)
 
 document.addEventListener('DOMContentLoaded', () => {
     ui.fillOptions(); // fill the select with years
@@ -68,19 +152,29 @@ function quoteInsurance(e) {
     const brand = document.querySelector('#brand').value;
 
     // read the selected year
-    const year = document.querySelector('#year').value;
+    const yeaR = document.querySelector('#year').value;
 
     // read the type of coverage
     const type = document.querySelector('input[name= "type"]:checked').value;
 
-    if (brand === '' || year === '' || type === '') {
+    if (brand === '' || yeaR === '' || type === '') {
         ui.showMessage('Todos los campos son obligatorios', 'error');
         return;
     }
 
-    ui.showMessage('Cotizando...', 'exito');
+    ui.showMessage('Cotizando...', 'sucess');
+
+    // hide previous quotations
+    const results = document.querySelector('#result div');
+    if (results != null){
+        results.remove();
+    }
 
     // instantiate insurance
+    const insurance = new Insurance(brand, yeaR, type);
+    const totality = insurance.quoteInsurance();
+
 
     // use the prototype to be quoted
+    ui.showResult(insurance, totality);
 }
